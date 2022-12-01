36cd8f2d9736cb16e9ddd745354bc3e7b059c69b
Marius Obert <obatzer@gmail.com>
2022-12-01 15:11:13 +0100

    Streamline with blog post

--- Readme.md
+++ Readme.md
@@ -5,7 +5,9 @@ With Twilio Media Streams, you can now extend the capabilities of your Twilio-po
 ## Blog Post
 If you prefer a step by step guide through building this yourself, this blog post will guide you through transcribing speech from a phone call into text, live in the browser using Twilio and Google Speech-to-Text using Node.js.
 
-[Visit the Blog Post here](https://www.twilio.com/blog/live-transcribing-phone-calls-using-twilio-media-streams-and-google-speech-text)
+[Visit the German blog post here](https://twilio.com/blog/anrufe-live-transkribieren)
+
+[Visit the original post here](https://www.twilio.com/blog/live-transcribing-phone-calls-using-twilio-media-streams-and-google-speech-text)
 
 ---
 
@@ -14,52 +16,58 @@ Before we can get started, you’ll need to make sure to have:
 
 - A [Free Twilio Account](https://www.twilio.com/try-twilio)
 - A [Google Cloud Account](https://cloud.google.com/)
-- Installed [ngrok](https://ngrok.com/)
-- Installed the [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart)
+- Install [ngrok](https://ngrok.com/)
+
+**Optional:** - Install the [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart)
 
 ---
 
 ## Setup
 
-1.  Setup Google Project and retrieve service account key
+1.  Setup Google Project and retrieve service account key via the [Cloud Console](https://console.cloud.google.com/) or the [Cloud SDK](https://cloud.google.com/sdk/docs/)
 
-    a. [Install and initialize the Cloud SDK](https://cloud.google.com/sdk/docs/)
+    a. Create a new GCP Project
 
-    b. Setup a new GCP Project
+    b. Enable the Google Speech-To-Text API for that project
 
-    c. Enable the Google Speech-To-Text API for that project
+    c. Create a service account.
 
-    d. Create a service account.
+    d. Create a private key as JSON.
 
-    e. Download a private key as JSON.
+    e. Download the key and add it as `key.json`.
 
-2.  Modify the `.env.sample` file to include the path to your JSON service account key and save it as a `.env` file
-3.  Run the following commands:
+2.  Buy a number either via the [Twilio Console](https://www.twilio.com/console/phone-numbers/search) or use the Twilio CLI with the following commands:
 
-    Buy a Phone Number (_I have used the `GB` country code to buy a mobile number, but feel free to change this for a [number local to you](https://support.twilio.com/hc/en-us/articles/223183068-Twilio-international-phone-number-availability-and-their-capabilities)._)
+    > For German numbers you need to create a [regulatory bundle](https://www.twilio.com/docs/phone-numbers/regulatory/getting-started/console-create-new-bundle) before as there are [country-specific guidlines](https://www.twilio.com/guidelines/de/regulatory) in place.
 
-    `$ twilio phone-numbers:buy:mobile --country-code GB`
+    Find a Phone Number (_I have used the `DE` country code to buy a mobile number, but feel free to change this for a [number local to you](https://support.twilio.com/hc/en-us/articles/223183068-Twilio-international-phone-number-availability-and-their-capabilities)._)
 
-    Start ngrok:
+    `twilio api:core:available-phone-numbers:mobile:list --country-code DE`
+
+    Buy a Phone Number 
+
+    ```
+    twilio api:core:incoming-phone-numbers:create --bundle-sid=BUxxxx \n
+      --address-sid=ADxxxx --friendly-name=cli-purchase \n 
+      --phone-number=<Number from the previous output>
+    ```
+
+
+3. Start ngrok with the following command
 
     `$ ngrok http 8080`
 
-    While this is running in a new window copy the forwarding HTTPS URL (https://xxxxx.ngrok.io) and set your Twilio number to this URL:
+4.  Copy the forwarding HTTPS URL (https://xxxxx.ngrok.io) and set your Twilio number to this URL in the Twilio Console or via the CLI:
 
-    `$ twilio phone-numbers:update TWILIO_NUMBER --voice-url https://xxxxxxxx.ngrok.io`
+    `$ twilio phone-numbers:update <TWILIO_NUMBER> --voice-url https://xxxxxxxx.ngrok.io`
 
     Install dependencies and start your server:
 
-    `$ npm install`
+5. Install the dependecies and start the application:
 
-    `$ npm start`
+    ```
+    $ npm install
+    $ npm start
+    ```
 
----
-
-# Contact me
-
-If you have any questions, feedback or just want to show me what you build, feel free to reach out to me:
-
-- Twitter: [@chatterboxcoder](https://twitter.com/chatterboxCoder)
-- GitHub: nokenwa
-- Email: nokenwa@twilio.com
+6. Last but not least, access the <http://localhost:8080> and call the number you bought.
--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const wss = new WebSocket.Server({ server });
-
 const path = require("path");
-
-require("dotenv").config();
 
 //Include Google Speech to Text
 const speech = require("@google-cloud/speech");
@@ -14,74 +11,72 @@ const client = new speech.SpeechClient();
 
 //Configure Transcription Request
 const request = {
-  config: {
-    encoding: "MULAW",
-    sampleRateHertz: 8000,
-    languageCode: "de-DE",
-  },
-  interimResults: true, // If you want interim results, set this to true
+    config: {
+        encoding: "MULAW",
+        sampleRateHertz: 8000,
+        languageCode: "de-DE"
+    },
+    interimResults: true
 };
 
 wss.on("connection", function connection(ws) {
-  console.log("New Connection Initiated");
+    console.log("Neue Verbindung initiiert");
 
-  let recognizeStream = null;
+    let recognizeStream = null;
 
-  ws.on("message", function incoming(message) {
-    const msg = JSON.parse(message);
-    switch (msg.event) {
-      case "connected":
-        console.log(`A new call has connected.`);
-        break;
-      case "start":
-        console.log(`Starting Media Stream ${msg.streamSid}`);
-        // Create Stream to the Google Speech to Text API
-        recognizeStream = client
-          .streamingRecognize(request)
-          .on("error", console.error)
-          .on("data", (data) => {
-            console.log(data.results[0].alternatives[0].transcript);
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(
-                  JSON.stringify({
-                    event: "interim-transcription",
-                    text: data.results[0].alternatives[0].transcript,
-                  })
-                );
-              }
-            });
-          });
-        break;
-      case "media":
-        // Write Media Packets to the recognize stream
-        recognizeStream.write(msg.media.payload);
-        break;
-      case "stop":
-        console.log(`Call Has Ended`);
-        recognizeStream.destroy();
-        break;
-    }
-  });
+    ws.on("message", function incoming(message) {
+        const msg = JSON.parse(message);
+        switch (msg.event) {
+            case "connected":
+                console.log(`Neuer Anruf verbunden`);
+
+                // Create Stream to the Google Speech to Text API
+                recognizeStream = client
+                    .streamingRecognize(request)
+                    .on("error", console.error)
+                    .on("data", data => {
+                        console.log(data.results[0].alternatives[0].transcript);
+                        wss.clients.forEach(client => {
+                            if (client.readyState === WebSocket.OPEN) {
+                                client.send(
+                                    JSON.stringify({
+                                        event: "interim-transcription",
+                                        text: data.results[0].alternatives[0].transcript
+                                    })
+                                );
+                            }
+                        });
+                    });
+                break;
+            case "start":
+                console.log(`Starte den Media-Stream ${msg.streamSid}`);
+                break;
+            case "media":
+                // Write Media Packets to the recognize stream
+                recognizeStream.write(msg.media.payload);
+                break;
+            case "stop":
+                console.log(`Anruf beendet`);
+                recognizeStream.destroy();
+                break;
+        }
+    });
 });
 
-app.use(express.static("public"));
-
+//Handle HTTP Request
 app.get("/", (_, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.post("/", (req, res) => {
-  res.set("Content-Type", "text/xml");
-
-  res.send(`
-    <Response>
-      <Start>
-        <Stream url="wss://${req.headers.host}/"/>
-      </Start>
-      <Say language="de-DE" >Hallo. Ich werde die Tonspur für die nächsten 60 Sekunden an einen WebSocket weiterstreamen.</Say>
-      <Pause length="60" />
-    </Response>
-  `);
+    res.set("Content-Type", "text/xml");
+    res.send(`
+      <Response>
+        <Start>
+         <Stream url="wss://${req.headers.host}/"/>
+        </Start>
+        <Say language="de-DE">Hallo. Ich werde die Tonspur für die nächsten 60 Sekunden an einen WebSocket weiterstreamen.</Say>
+        <Pause length="60" />
+      </Response>`);
 });
 
-console.log("Listening on Port 8080");
+console.log("Gestartet auf Port 8080");
 server.listen(8080);
